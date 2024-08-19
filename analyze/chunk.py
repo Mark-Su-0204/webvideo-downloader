1a175f10e1d721dd0c1e1734af1f5b286e42cc5a
Mark-Su-0204 <marksumuhan@qq.com>
2024-08-19 10:08:27 +0800

    draw chunk length distribution with video categories

--- analyze/chunk.py
+++ analyze/chunk.py
@@ -1,6 +1,8 @@
 import os
 import json
 import m3u8
+from matplotlib import pyplot as plt
+from matplotlib import font_manager
 
 # Recursively find all values with a certain key name in json
 def find_key_in_json(json_data, target_key, res):
@@ -17,21 +19,9 @@ def find_key_in_json(json_data, target_key, res):
     return res
 
 
-# For IQY, content of the m3u8 file is contained in json
-# Input: path to json file
-# Output: m3u8.M3U8
-def extract_m3u8_iqy(filepath):
-    with open(filepath, 'r') as f:
-        j = json.load(f)
-    manifest = find_key_in_json(j, 'm3u8', [])
-    manifest = m3u8.loads(manifest[0])
-    return manifest
-
-
-# For Tencent, the m3u8 file is pre-downloaded
 # Input: path to m3u8 file
 # Output: m3u8.M3U8
-def extract_m3u8_tencent(filepath):
+def extract_m3u8(filepath):
     with open(filepath, 'r') as f:
         manifest = f.read()
     manifest = m3u8.loads(manifest)
@@ -46,8 +36,42 @@ def parse_m3u8(manifest:m3u8.M3U8):
     return chunks
 
 
-filepath = '../requests/dash.json'
-filepath = '../requests/gzc_1000035_0bc3fuaqaaab3aacqpvhaztjilodaawqcaca.f306310.ts.m3u8'
-manifest = extract_m3u8_tencent(filepath)
-chunks = parse_m3u8(manifest)
-print(chunks)
+def draw_chunk_distribution(videos, names, graph_dir, category, app):
+    if len(videos) == 0:
+        return
+    positions = [i for i in range(len(videos))]
+    plt.violinplot(videos, positions=positions, showmeans=True)
+    plt.title('Violin Plot Example')
+    plt.xlabel('Name')
+    plt.ylabel('Chunk lengths')
+    # plt.xticks(positions, names)
+    filepath = os.path.join(graph_dir, app + '_' + category + '.pdf')
+    plt.savefig(filepath)
+    plt.clf()
+
+
+def draw_category(app_dir, category, app):
+    dir = os.path.join(app_dir, category)
+    manifests = sorted(os.listdir(dir))
+    videos = []
+    names = []
+    for manifest in manifests:
+        if not manifest.endswith('m3u8'):
+            continue
+        path = os.path.join(dir, manifest)
+        video = parse_m3u8(extract_m3u8(path))
+        name = manifest[:-5]
+        name = name.split('_')[-1]
+        videos.append(video)
+        names.append(name)
+    draw_chunk_distribution(videos, names, graph_dir, category, app)
+
+
+app = 'Tencent'
+graph_dir = '../graph/chunk_dist'
+app_dir = '../manifests/' + app
+categories = os.listdir(app_dir)
+for category in categories:
+    if category == '.DS_Store':
+        continue
+    draw_category(app_dir, category, app)
